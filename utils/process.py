c61a8d660ef71d64e113bac7cb49d88eece30cb9
Tingmengyu28 <1244419066@qq.com>
2024-10-21 16:29:15 +0800

    Remove some useless information

--- utils/process.py
+++ utils/process.py
@@ -45,10 +45,11 @@ class LitDDNet(pl.LightningModule):
             output_aif_images = self.rnet(images, output_depths)
             output_depths = output_depths.squeeze()
             output_images, _ = self.generator(output_aif_images, output_depths, focal_distance, aperture, focal_length)
+            
             # L_depth = l1_norm(output_depths, depths) + tv_norm(output_depths, self.args['lambda0'])
             # L_image = l1_norm(aif_images, output_aif_images, self.args['alpha']) + (1 - cal_ssim(output_aif_images, aif_images)) * self.args['lambda1']
             # L_rec = l1_norm(output_images, images, self.args['gamma'])
-            # L_depth = mean_square_error(depths, output_depths, self.args['mu'])
+            
             L_depth = kl_inverse_gamma(depths, output_depths, self.args['mu']) + tv_norm(output_depths, 0.001)
             L_image = mean_square_error(aif_images, output_aif_images, self.args['alpha'])
             L_rec = mean_square_error(output_images, images, self.args['gamma'])
@@ -65,15 +66,6 @@ class LitDDNet(pl.LightningModule):
             L_rec = l1_norm(output_images, images, self.args['gamma'])
             
             loss = L_depth + 0.001 * L_image + L_rec
-
-        elif self.args['model_name'] == 'D3Net':
-            output_depths, output_aif_images = self(images)
-            output_depths = output_depths.squeeze()
-            output_images, _ = self.generator(output_aif_images, output_depths, focal_distance, aperture, focal_length)
-
-            loss = l1_norm(output_depths, depths) + tv_norm(output_depths, self.args['lambda0']) + \
-                   l1_norm(output_images, images, self.args['gamma']) + \
-                   l1_norm(aif_images, output_aif_images, self.args['alpha']) + (1 - cal_ssim(output_aif_images, aif_images)) * self.args['lambda1']
 
         self.log('train_loss', loss)
         return loss
@@ -93,13 +85,7 @@ class LitDDNet(pl.LightningModule):
 
         images, betas = self.generator(aif_images, depths, focal_distance, aperture, focal_length)
 
-        if self.args['model_name'] in ['AttnNet']:
-            output_betas, output_depths = self(images)
-            output_betas = output_betas.squeeze()
-            output_depths = output_depths.squeeze()
-            output_aif_images = aif_images
-
-        elif self.args['model_name'] in ['2HDEDNet', 'D3Net']:
+        if self.args['model_name'] == '2HDEDNet':
             output_depths, output_aif_images = self(images)
             output_depths = output_depths.squeeze()
             
@@ -141,35 +127,22 @@ class LitDDNet(pl.LightningModule):
             L_rec = mean_square_error(output_images, images, self.args['gamma'])
 
             loss = L_depth + L_image + L_rec
-            
+
         elif self.args['model_name'] == '2HDEDNet':
             output_depths, output_aif_images = self(images)
             output_depths = output_depths.squeeze()
             output_images, _ = self.generator(aif_images, output_depths, focal_distance, aperture, focal_length)
-            
+
             L_depth = l1_norm(output_depths, depths) + tv_norm(output_depths, 0.01)
             L_image = CharbonnierLoss(aif_images, output_aif_images) + (1 - cal_ssim(output_aif_images, aif_images)) * 4
             L_rec = l1_norm(output_images, images, self.args['gamma'])
-            
+
             loss = L_depth + 0.001 * L_image + L_rec
-
-        elif self.args['model_name'] == 'D3Net':
-            output_depths, output_aif_images = self(images)
-            output_depths = output_depths.squeeze()
-            output_images, _ = self.generator(output_aif_images, output_depths, focal_distance, aperture, focal_length)
-
-            loss = l1_norm(output_depths, depths) + tv_norm(output_depths, self.args['lambda0']) + \
-                   l1_norm(output_images, images, self.args['gamma']) + \
-                   l1_norm(aif_images, output_aif_images, self.args['alpha']) + (1 - cal_ssim(output_aif_images, aif_images)) * self.args['lambda1']
 
         self.log('val_loss', loss)
         self.log('val_rmse_depth', self.args['depth_max'] * mean_square_error(output_depths, depths) ** 0.5)
         self.log('val_ssim_image', cal_ssim(aif_images, output_aif_images))
 
-        if batch_idx == 10:
-            if self.args['model_name'] in ['DRNet', 'D3Net']:
-                visualize_sample(images[0], output_images[0], aif_images[0], output_aif_images[0], depths[0], output_depths[0], self.logger, self.global_step)
-            elif self.args['model_name'] == '2HDEDNet':
-                visualize_sample(images[0], output_images[0], aif_images[0], output_aif_images[0], depths[0], output_depths[0], self.logger, self.global_step)
-
+        if batch_idx == 8 and self.args['model_name'] in ['DRNet', '2HDEDNet']:
+            visualize_sample(images[0], output_images[0], aif_images[0], output_aif_images[0], depths[0], output_depths[0], self.logger, self.global_step)
         return loss
